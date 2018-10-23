0cd4c75deb4e20cfee0f67b1b0fdd8e790cda310
Marc MICHIELI <zyx1@free.fr>
2018-10-23 20:48:07 +0200

    Initial commit

---
+++ src/main/java/com/mmi/art/first/firstmicroservices/FirstMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.mmi.art.first.firstmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMicroservicesApplication.class, args);
+	}
+}
