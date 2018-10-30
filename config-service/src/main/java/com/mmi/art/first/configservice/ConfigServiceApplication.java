957e613a61a75936e3d530879c8af4de27932310
Marc MICHIELI <zyx1@free.fr>
2018-10-31 00:02:04 +0100

    Configuration Service Fix

--- config-service/src/main/java/com/mmi/art/first/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/mmi/art/first/configservice/ConfigServiceApplication.java
@@ -2,13 +2,15 @@ package com.mmi.art.first.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Centralize the configuration for microservices
  */
-@EnableConfigServer
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
