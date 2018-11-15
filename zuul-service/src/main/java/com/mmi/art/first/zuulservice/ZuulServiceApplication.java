517e2fd04585ab68fec4a4beb07e87ff88e7c98f
Marc MICHIELI <zyx1@free.fr>
2018-11-15 23:44:19 +0100

    Hystrix and Ribbon OK

--- zuul-service/src/main/java/com/mmi/art/first/zuulservice/ZuulServiceApplication.java
+++ zuul-service/src/main/java/com/mmi/art/first/zuulservice/ZuulServiceApplication.java
@@ -3,16 +3,19 @@ package com.mmi.art.first.zuulservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * API-Gateway for First Microservices
  * @link @EnableZuulProxy turns this application as Netflix Zuul Gateway
  * @link @EnableDiscoveryClient allows this app to register with Discovery
+ * @link @EnableHystrix allows this app to be managed by Hystrix
  */
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableHystrix
 public class ZuulServiceApplication {
 
 	public static void main(String[] args) {
